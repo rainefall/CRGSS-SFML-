05e30fc2685ae59e4e546ebd75bc8da4794fd66b
Toby <sukoshijon@gmail.com>
2018-02-24 19:42:54 +0000

    Organized everything a bit

--- crgss.cr
+++ crgss.cr
@@ -1,8 +1,9 @@
 require "crsfml"
 
 require "./src/crgss.cr"
-require "./src/bitmap.cr"
-require "./src/sprite.cr"
-require "./src/sprite3d.cr"
-require "./src/graphics.cr"
+require "./src/graphics/rect.cr"
+require "./src/graphics/bitmap.cr"
+require "./src/graphics/sprite.cr"
+require "./src/graphics/3D/sprite3d.cr"
+require "./src/graphics/graphics.cr"
 
