aa189809c0a183c9271dff1ef0476f273833cacd
Toby <sukoshijon@gmail.com>
2018-02-24 19:45:21 +0000

    Fixed a bug where crsfml.cr would look for the graphics code in a non existant folder

--- crgss.cr
+++ crgss.cr
@@ -1,9 +1,9 @@
 require "crsfml"
 
 require "./src/crgss.cr"
-require "./src/graphics/rect.cr"
-require "./src/graphics/bitmap.cr"
-require "./src/graphics/sprite.cr"
-require "./src/graphics/3D/sprite3d.cr"
-require "./src/graphics/graphics.cr"
+require "./src/Graphics/rect.cr"
+require "./src/Graphics/bitmap.cr"
+require "./src/Graphics/sprite.cr"
+require "./src/Graphics/3D/sprite3d.cr"
+require "./src/Graphics/graphics.cr"
 
