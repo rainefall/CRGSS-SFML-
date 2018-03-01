e0a23fb947b7f5ee928e7e5e2da59b59fc540878
Toby <sukoshijon@gmail.com>
2018-03-01 12:13:11 +0000

    Primitives actually work now

--- crgss.cr
+++ crgss.cr
@@ -1,9 +1,23 @@
 require "crsfml"
 
+# Main module
 require "./src/crgss.cr"
+
+# Utilities
 require "./src/Graphics/rect.cr"
+require "./src/Graphics/color.cr"
+
+# Bitmaps/Models
 require "./src/Graphics/bitmap.cr"
+
+# Primitives
+require "./src/Graphics/Primitives/circle.cr"
+require "./src/Graphics/Primitives/rectangle.cr"
+
+# Sprites
 require "./src/Graphics/sprite.cr"
 require "./src/Graphics/3D/sprite3d.cr"
+
+# Modules
 require "./src/Graphics/graphics.cr"
 
