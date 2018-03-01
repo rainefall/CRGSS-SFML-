bfd2357f1d076ee66b4d28917ec7de46bc228ea9
Toby <sukoshijon@gmail.com>
2018-03-01 12:26:29 +0000

    Examples work now

--- examples/run.sh
+++
@@ -1,4 +0,0 @@
-export LIBRARY_PATH=./lib/voidcsfml
-export LD_LIBRARY_PATH="$LIBRARY_PATH"
-
-crystal test.cr
--- src/Graphics/graphics.cr
+++ src/Graphics/graphics.cr
@@ -5,16 +5,23 @@ module CRGSS
 
         # Make a new window
         @@window = SF::RenderWindow.new(SF::VideoMode.new(640,400),"CRGSS")
+        @@render_target = SF::RenderTexture.new(640,400)
         self.window.vertical_sync_enabled = true
 
         def self.window
             return @@window
         end
 
+        def self.render_target
+            return @@render_target
+        end
+
         def self.update
             CRGSS.update
             self.window.clear(SF::Color::Black)
             CRGSS.resources.each { |i| i.draw(self.window)}
+            #target = sprite = SF::Sprite.new(self.render_target.texture.load_from_image(self.render_target.texture.copy_to_image.flip_vertically))
+            #self.window.draw(target)
             self.window.display
         end
     end
